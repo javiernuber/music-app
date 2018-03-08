41e77252baf012f7897e202c2c4809264d0b160f
Javier Núñez <javi@aspgems.com>
2018-03-08 18:08:50 +0100

    Mixins test

--- src/components/TrackDetail.vue
+++ src/components/TrackDetail.vue
@@ -1,16 +1,36 @@
 <template lang="pug">
   .container
     .columns
-      .column.is-5.is-offset-4
-        pm-track(:track="track")
+      .column.is-3.has-text-centered
+        figure.media-left
+          p.image(v-if="track && track.album")
+            img(:src="track.album.images[0].url")
+          p
+            a.button.is-primary.is-large(@click="selectTrack") Seleccionar
+      .column.is-8
+        .panel
+          .panel-heading
+            h1.title {{ track.name }}
+          .panel-block
+            article.media
+              .media-content
+                .content
+                  ul
+                    li(v-for="(v, k) in track")
+                      strong {{ k }}: &nbsp;
+                      span {{ v }}
+              nav.level
+                .level-left
+                  a.level-item
 </template>
 
 <script>
 import trackService from '@/services/track'
-import PmTrack from '@/components/Track'
+import trackMixin from '@/mixins/track'
 
 export default {
-  components: { PmTrack },
+  mixins: [ trackMixin ],
+
   data () {
     return {
       track: {}
